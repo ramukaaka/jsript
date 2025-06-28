c8c593d9aba18cb77f8242baf68479de5f61f9fc
ramukaaka <128740572+ramukaaka@users.noreply.github.com>
2025-06-28 14:58:38 +0000

    first commit

---
+++ basic/01_variable.js
@@ -0,0 +1,26 @@
+let number = 10
+const name =  "Peter" 
+ 
+
+
+
+ console.log(name)
+ console.log(number)
+
+ console.log(typeof(name))
+
+number = 20;
+// let name =ashu ; const can't be changed
+
+
+
+ console.log("-------------------------------------------------")
+
+ console.log(name)
+ console.log(number)
+
+ age = 15
+ 
+ console.log(typeof age);
+ 
+
---
+++ basic/03_comparison.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//console.log( 2>1)
+//console.log( 1<1)
+//console.log( 2==1)
+//console.log( 2!=1)
+//console.log( 2<=1)
+//console.log( 2>=1)
+
+console.log("2"== 2)
+console.log("2"=== 2) // === don;t convert the data type
+console.log("2"<1);
+console.log("2">1);
+
+
+console.log("-----null comparison --------")
+console.log(null==0)
+console.log(null<=0)
+console.log(null>=0)
+
+
+console.log("-----undefined comparison --------")
+console.log(undefined==0)
+console.log(undefined<=0)
+console.log(undefined>=0)
+
